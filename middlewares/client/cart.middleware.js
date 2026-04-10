1354f3b9a7d065e07e67eadc7d52984efdac78c7
Nguyễn Hữu Tú <huutu2503@gmail.com>
2026-04-10 11:44:28 +0700

    mini-cart

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -1,6 +1,6 @@
 const Cart = require("../../models/cart.model");
 
-module.exports.cartId = (req, res, next) => {
+module.exports.cartId = async (req, res, next) => {
   if (!req.cookies.cartId) {
     const cart = new Cart();
     cart.save();
@@ -9,7 +9,12 @@ module.exports.cartId = (req, res, next) => {
       expires: new Date(Date.now() + time)
     })
   } else {
-
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId
+    });
+    const totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
+    cart.totalQuantity = totalQuantity;
+    res.locals.miniCart = cart;
   }
   next();
 }
